1dd7874d1d47f2973d4c53c4a30ca747ed0c4d48
suzyrgo <suztra0803@skola.goteborg.se>
2021-03-11 21:59:23 +0100

    collected data from r and tested a few cities

--- smhi/shmi.js
+++ smhi/shmi.js
@@ -1,18 +1,15 @@
-// NB! This code is not very good by design
-// it should not be too copy-paste friendly =)
-
 const BASE_URL = "https://opendata-download-metfcst.smhi.se/api";
 
 const cities = [
   { name: "Göteborg", lng: 16.158, lat: 58.5812 },
   { name: "Helsingborg", lng: 12.68, lat: 56.04 },
-  { name: "Jönköping", lng: 14.17, lat: 57.78 },
-  { name: "Linköping", lng: 15.67, lat: 58.38 },
-  { name: "Malmö", lng: 13.00073, lat: 55.60587 },
-  { name: "Stockholm", lng: 18.0649, lat: 59.33258 },
-  { name: "Uppsala", lng: 17.7, lat: 59.87 },
-  { name: "Västerås", lng: 16.55, lat: 59.6 },
-  { name: "Örebro", lng: 15.18, lat: 59.28 },
+  //{ name: "Jönköping", lng: 14.17, lat: 57.78 },
+  //{ name: "Linköping", lng: 15.67, lat: 58.38 },
+  //{ name: "Malmö", lng: 13.00073, lat: 55.60587 },
+  //{ name: "Stockholm", lng: 18.0649, lat: 59.33258 },
+  //{ name: "Uppsala", lng: 17.7, lat: 59.87 },
+  //{ name: "Västerås", lng: 16.55, lat: 59.6 },
+  //{ name: "Örebro", lng: 15.18, lat: 59.28 },
 ];
 
 /*
@@ -61,7 +58,7 @@ function findHighAndLowTemp(forecast) {
 
 function findTemperature(parameters) {
   for (const param of parameters) {
-    if (param.name === "t") {
+    if (param.name === "r") {
       return param.values[0];
     }
   }
